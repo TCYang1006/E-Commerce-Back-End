8a8e1411c762f5f12dfc5f75c519655af1d22dcf
Peter Thao <peterthao@Peters-iMac.local>
2021-11-03 21:33:51 -0400

    category routes updated

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -3,26 +3,53 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get('/api/categories', (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  Post.findAll({}).then(dbCategories => {
+    res.json(dbCategories);
+  })
+
 });
 
-router.get('/:id', (req, res) => {
+router.get('/api/category/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  Post.findAll({
+    where: {
+      id: req.params.id
+    }
+  }).then((dbCategories) => res.json(dbCategories))
 });
 
-router.post('/', (req, res) => {
+router.post('/api/category', (req, res) => {
   // create a new category
+  Post.create({
+    where: {
+      title: req.body.title,
+      body: req.body.body,
+      category: req.body.category
+    }
+  }).then(dbCategories => res.json(dbCategories))
 });
 
-router.put('/:id', (req, res) => {
+router.put('/api/category/:id', (req, res) => {
   // update a category by its `id` value
+  Post.update({
+    where: {
+      id: req.params.id
+    }
+  }).then(dbCategories => res.json(dbCategories))
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/api/category/:id', (req, res) => {
   // delete a category by its `id` value
+  Post.destroy({
+    where: {
+      id: req.params.id
+    }
+  }).then(dbCategories => res.json(dbCategories))
+
 });
 
 module.exports = router;
