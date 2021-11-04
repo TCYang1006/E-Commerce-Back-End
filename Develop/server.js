cda3588107db18cdbd51735f009c46609e1f749f
Peter Thao <peterthao@Peters-iMac.local>
2021-11-03 20:56:37 -0400

    defined routes and sync seq

--- Develop/server.js
+++ Develop/server.js
@@ -9,9 +9,17 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(routes);
+//routes
+app.use(require('./routes/api/category-routes'));
+app.use(require('./routes/api/product-routes'));
+app.use(require('./routes/api/tag-routes'));
 
 // sync sequelize models to the database, then turn on the server
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}!`);
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}!`);
+  });
 });
+
+
